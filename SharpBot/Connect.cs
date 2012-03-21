2abe6fbeae6bcb0837d042765741de319f586e19
tomjones <tomjones@tomjones-loves-fedora.rackspace.corp>
2012-03-21 15:09:26 -0400

    Initial Commit

---
+++ SharpBot/Connect.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+
+using Meebey.SmartIrc4net;
+namespace SharpBot
+{
+	public class Connect
+	{
+		private static IrcClient client;
+		public static IrcClient Client
+		{
+			get
+			{
+				if(client==null)
+				{
+					client = new IrcClient();
+				}
+				return client;
+			}
+			set
+			{
+				client = value;
+			}
+		}
+		
+		public string ServerName {get;set;}
+		public int Port {get;set;}
+		public string DefaultChannel {get;set;}
+		
+		public Connect ()
+		{
+			Client.Encoding = System.Text.Encoding.UTF8;
+	        
+	        // wait time between messages, we can set this lower on own irc servers
+	        Client.SendDelay = 200;
+	        
+	        // we use channel sync, means we can use irc.GetChannel() and so on
+	        Client.ActiveChannelSyncing = true;
+		}
+		
+			
+		public void Init()
+		{
+			try
+			{
+				Client.Connect(ServerName,Port);
+				connect();
+			}
+		    catch (ConnectionException e) 
+		    {  
+            // something went wrong, the reason will be shown
+            System.Console.WriteLine("couldn't connect! Reason: "+e.Message);
+            Exit();
+			}
+		}
+		
+		private void connect()
+		{
+			try {
+            // here we logon and register our nickname and so on 
+            Client.Login("SmartIRC", "SmartIrc4net Test Bot");
+            // join the channel
+            Client.RfcJoin(DefaultChannel);
+            
+            for (int i = 0; i < 3; i++) {
+                // here we send just 3 different types of messages, 3 times for
+                // testing the delay and flood protection (messagebuffer work)
+                Client.SendMessage(SendType.Message, DefaultChannel, "test message ("+i.ToString()+")");
+                Client.SendMessage(SendType.Action, DefaultChannel, "thinks this is cool ("+i.ToString()+")");
+                Client.SendMessage(SendType.Notice, DefaultChannel, "SmartIrc4net rocks ("+i.ToString()+")");
+            }
+            
+            // spawn a new thread to read the stdin of the console, this we use
+            // for reading IRC commands from the keyboard while the IRC connection
+            // stays in its own thread
+            new Thread(new ThreadStart(ReadCommands)).Start();
+            
+            // here we tell the IRC API to go into a receive mode, all events
+            // will be triggered by _this_ thread (main thread in this case)
+            // Listen() blocks by default, you can also use ListenOnce() if you
+            // need that does one IRC operation and then returns, so you need then 
+            // an own loop 
+            Client.Listen();
+            
+            // when Listen() returns our IRC session is over, to be sure we call
+            // disconnect manually
+            Client.Disconnect();
+        } catch (ConnectionException) {
+            // this exception is handled because Disconnect() can throw a not
+            // connected exception
+            Exit();
+        } catch (Exception e) {
+            // this should not happen by just in case we handle it nicely
+            System.Console.WriteLine("Error occurred! Message: "+e.Message);
+            System.Console.WriteLine("Exception: "+e.StackTrace);
+            Exit();
+        }
+		}
+		
+			public static void ReadCommands()
+	    {
+	        // here we read the commands from the stdin and send it to the IRC API
+	        // WARNING, it uses WriteLine() means you need to enter RFC commands
+	        // like "JOIN #test" and then "PRIVMSG #test :hello to you"
+	        while (true) {
+	            string cmd = System.Console.ReadLine();
+	            if (cmd.StartsWith("/list")) {
+	                int pos = cmd.IndexOf(" ");
+	                string channel = null;
+	                if (pos != -1) {
+	                    channel = cmd.Substring(pos + 1);
+	                }
+	                
+	                IList<ChannelInfo> channelInfos = Client.GetChannelList(channel);
+	                Console.WriteLine("channel count: {0}", channelInfos.Count);
+	                foreach (ChannelInfo channelInfo in channelInfos) {
+	                    Console.WriteLine("channel: {0} user count: {1} topic: {2}",
+	                                      channelInfo.Channel,
+	                                      channelInfo.UserCount,
+	                                      channelInfo.Topic);
+	                }
+	            } else {
+	                Client.WriteLine(cmd);
+	            }
+	        }
+	    }
+	    
+	    public static void Exit()
+	    {
+	        // we are done, lets exit...
+	        System.Console.WriteLine("Exiting...");
+	        System.Environment.Exit(0);
+	    }
+		}
+	}
+
